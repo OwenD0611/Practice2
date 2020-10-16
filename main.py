bc0dc67e3a402d8aaf35dd3cc177d8e2b929647a
OwenD0611 <551555@my.cuhsd.org>
2020-10-16 18:59:12 +0000

    draw the axes function.

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import turtle as trtl
 
-#draw a curved line made up of 4 lines
+#draws a curve left 90 degrees
 trtl.pendown()
-for i in range(4):
-  trtl.forward(50)
-  trtl.left(20)
+for i in range(6):
+  trtl.forward(10)
+  trtl.left(15)
 trtl.penup()
+
+def draw_axes():
+  trtl.goto(0,0)
+  trtl.pendown()
+  trtl.forward(100)
+  trtl.backward(200)
+  trtl.forward(100)
+  trtl.left(90)
+  trtl.forward(100)
+  trtl.backward(200)
+  trtl.pendown()
+  trtl.goto(0,0)
+
+draw_axes()
+
